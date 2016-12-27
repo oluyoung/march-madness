86e7b28cb827206e72dd2eef736024de027d0131
kanyin-me <kanyinsola_olugbake@yahoo.co.uk>
2016-12-27 03:03:56 +0000

    initial commit

---
+++ header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> >
+<head>
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <meta name=viewport content="width=device-width">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title><?php bloginfo('title'); wp_title(); ?></title>
+  <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+<section id=page>
+<header id=site-header>
+  <div class=hd-top>
+    <div class="hd-search">
+      <?php get_search_form( ); ?>
+    </div>
+    <h1 class=hd-title>
+      <a href="<?php echo home_url(); ?>"><?php bloginfo('name');?></a>
+    </h1>
+  </div>
+  <nav class=site-nav>
+    <?php //nav menu ?>
+  </nav>
+
+</header>
