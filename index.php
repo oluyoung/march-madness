1cf797cadd1aa7ca0c28b1cfc53a4f9f688b5404
kanyin-me <kanyinsola_olugbake@yahoo.co.uk>
2017-01-05 00:23:11 +0000

    some stuff done

--- index.php
+++ index.php
@@ -13,6 +13,15 @@
       echo "<h4>No content found</h4>";
     endif;
     ?>
+
+    <!-- Pagination -->
+    <?php
+    the_posts_pagination( array(
+        'mid_size' => 2,
+        'prev_text' => __( '<span class="multi-posts-nav">Previous</span>', 'textdomain' ),
+        'next_text' => __( '<span class="multi-posts-nav">Next</span>', 'textdomain' ),
+    ) );
+    ?>
   </section>
 
   <?php get_sidebar(); ?>
